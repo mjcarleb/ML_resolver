6a4daf8e4fa1a207459012834dcb64af04d3ed02
Mark Carlebach <markcarlebach@macbook-pro.myfiosgateway.com>
2022-05-12 06:15:09 -0400

    fit first model

--- train.py
+++ train.py
@@ -18,16 +18,15 @@ label = "resolver_label"
 
 feature_enc = OneHotEncoder()
 feature_fit = feature_enc.fit(X=train[features])
-train_X = feature_enc.transform(X=train[features])
+X_tr = feature_enc.transform(X=train[features])
 
 label_enc = OneHotEncoder()
 label_fit = label_enc.fit(X=train[label].values.reshape(-1, 1))
-train_y = label_enc.transform(X=train[label].values.reshape(-1, 1))
+y_tr = label_enc.transform(X=train[label].values.reshape(-1, 1))
 
 ########################################
 #     TRAIN MODEL
 ########################################
 clf = DecisionTreeClassifier(random_state=42)
 
-model = clf.fit(X=train_X.toarray(), y=train_y.toarray())
-a=3
+model = clf.fit(X=X_tr.toarray(), y=y_tr.toarray())
