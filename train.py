9507648ab178c8500cf554615e74bab38de56534
Mark Carlebach <markcarlebach@macbook-pro.myfiosgateway.com>
2022-05-12 07:03:25 -0400

    getting 100% with 2 features

--- train.py
+++ train.py
@@ -2,13 +2,14 @@ import sklearn
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 import pandas as pd
 
 data_dir = "../DataGeneration/data/"
 file_name = "training_trades"
 df = pd.read_parquet(path=f"{data_dir}{file_name}")
 
-train, test = train_test_split(df,test_size=0.15, random_state=42)
+train, val = train_test_split(df,test_size=0.15, random_state=42)
 
 ########################################
 #     PREPARE DATA
@@ -16,17 +17,25 @@ train, test = train_test_split(df,test_size=0.15, random_state=42)
 features = ["market", "source_system"]
 label = "resolver_label"
 
-feature_enc = OneHotEncoder()
+feature_enc = OneHotEncoder(sparse=False)
 feature_fit = feature_enc.fit(X=train[features])
 X_tr = feature_enc.transform(X=train[features])
+X_val = feature_enc.transform(X=val[features])
 
-label_enc = OneHotEncoder()
+label_enc = OneHotEncoder(sparse=False)
 label_fit = label_enc.fit(X=train[label].values.reshape(-1, 1))
 y_tr = label_enc.transform(X=train[label].values.reshape(-1, 1))
+y_val = label_enc.transform(X=val[label].values.reshape(-1, 1))
 
 ########################################
 #     TRAIN MODEL
 ########################################
 clf = DecisionTreeClassifier(random_state=42)
+model = clf.fit(X=X_tr, y=y_tr)
 
-model = clf.fit(X=X_tr.toarray(), y=y_tr.toarray())
+########################################
+#     EVALUATE THE MODEL
+########################################
+predictions = clf.predict(X_val)
+accuracy = accuracy_score(y_true=y_val, y_pred=predictions)
+print(f"Accuracy with features = {features} = {accuracy :0.2%}")
